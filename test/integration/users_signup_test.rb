7a22cf5d7f2abac4cf525fe171fa115a4297f941
Martin Moreau <martinmoreau@gmail.com>
2016-04-28 14:30:19 +0000

    Debug flash hash

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,5 +11,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_not flash.empty?
   end
 end
